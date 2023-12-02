4891851638fa02a262d65b4d5ad67e52ac359231
Thiago Lino <thiagolinu@gmail.com>
2023-12-02 11:01:47 -0300

    Refatorando o component Heading

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -21,7 +21,7 @@ describe('<Heading />', () => {
   it('should render a heading with a line to left side', () => {
     renderWithTheme(<Heading $lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+      'border-left': '0.7rem solid #F231A5'
     })
   })
 
@@ -32,6 +32,48 @@ describe('<Heading />', () => {
     ).not.toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
       modifier: '::after',
       media: 'screen'
+    })
+  })
+
+  it('should render a heading with small size', () => {
+    renderWithTheme(<Heading $size="small">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '1.6rem'
+    })
+
+    expect(
+      screen.getByRole('heading', { name: /won games/i })
+    ).not.toHaveStyleRule('border-bottom', '0.3rem', {
+      modifier: '::after',
+      media: 'screen'
+    })
+  })
+
+  it('should render a Heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading $lineColor="primary" $lineLeft $lineBottom>
+        Lorem Ipsum
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
+  })
+
+  it('should render a Heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading $lineColor="secondary" $lineLeft $lineBottom>
+        Lorem Ipsum
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier: '::after'
     })
   })
 })
